27005828142a8ddf7a1e7314d647effa52f1d677
mart cube <martingjorceski@gmail.com>
2023-12-27 22:52:03 +0100

    error page styles

--- src/error.vue
+++ src/error.vue
@@ -14,9 +14,26 @@ const handleError = () => clearError({ redirect: '/' })
 </script>
 
 <template>
-  <div>
-		<NuxtImg src="/chopper.png" />
-    <h2>{{ error.statusCode }}</h2>
-    <button @click="handleError">Clear errors</button>
+  <div class="error">
+		<NuxtImg src="/chopper.png"/>
+    <h2>Error {{ error.statusCode }}</h2>
+    <button @click="handleError">back to home</button>
   </div>
 </template>
+
+<!-- eslint-disable no-tabs -->
+<style lang="postcss" scoped>
+.error{
+	@apply w-full h-screen flex flex-col justify-center items-center gap-[1rem];
+	img{
+		@apply w-[10rem];
+	}
+	h2{
+		@apply text-title;
+	}
+	button{
+		@apply w-[10rem] h-[2rem] text-base capitalize;
+		@apply border-l-[1px] border-r-[1px] border-solid border-white;
+	}
+}
+</style>
