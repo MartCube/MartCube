a44b5691359dde4ef8430c8be2a23d33e1963d9d
mart cube <martingjorceski@gmail.com>
2024-07-09 18:09:57 +0200

    articleList animation update

--- src/components/SanityBlocks/BlogSection/ArticleList.vue
+++ src/components/SanityBlocks/BlogSection/ArticleList.vue
@@ -9,14 +9,35 @@ defineProps<{
 
 <template>
   <ul
+    v-auto-animate
     class="slide-enter w-full flex flex-wrap justify-between gap-2rem"
     lg="lg:flex-row gap-0"
   >
     <li
-      v-for="item in data"
+      v-for="(item, index) in data"
       :key="item.uid"
+      :style="{ transitionDelay: `${index * 0.1}s` }"
     >
       <ArticleCard :data="item" />
     </li>
   </ul>
 </template>
+
+<style>
+.fade-translate-enter-active,
+.fade-translate-leave-active {
+  transition: opacity 0.5s ease, transform 0.5s ease;
+}
+
+.fade-translate-enter-from,
+.fade-translate-leave-to {
+  opacity: 0;
+  transform: translateY(10px);
+}
+
+.fade-translate-enter-to,
+.fade-translate-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
