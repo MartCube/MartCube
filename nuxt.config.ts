dc87eeffb363a992b579cb7e4dc091497ca291fd
mart cube <martingjorceski@gmail.com>
2024-07-10 15:05:13 +0200

    prerender hooks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  // ssr: false,
+  hooks: {
+    'prerender:routes'({ routes }) {
+      routes.clear() // Do not generate any routes (except the defaults)
+    },
+  },
   devtools: { enabled: true },
   srcDir: 'src',
   css: ['~/assets/base.css'],
