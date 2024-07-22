f25ed3994bded5886b54f7a0ccf7cbf9665f8a77
mart cube <martingjorceski@gmail.com>
2024-07-22 14:34:13 +0200

    blockquote and image caption

--- src/components/SanityBlocks/ArticleSection/ImageBlock.vue
+++ src/components/SanityBlocks/ArticleSection/ImageBlock.vue
@@ -1,18 +1,24 @@
 <script lang="ts" setup>
 defineProps<{
   src: string
+  caption: string
 }>()
 </script>
 
 <template>
-  <NuxtImg
-    class="h-auto max-w-[1280px] w-full object-cover"
-    :src="src"
-    width="1280"
-    height="720"
-    fit="max"
-    loading="lazy"
-    provider="sanity"
-    placeholder
-  />
+  <figure>
+    <NuxtImg
+      class="h-auto max-w-[1280px] w-full object-cover"
+      :src="src"
+      width="1280"
+      height="720"
+      fit="max"
+      loading="lazy"
+      provider="sanity"
+      placeholder
+    />
+    <figcaption class="text-center text-primary italic">
+      "<span class="text-small">{{ caption }}</span>"
+    </figcaption>
+  </figure>
 </template>
